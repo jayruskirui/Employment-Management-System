f6dc75d1cfe0499437ca580acab9928de1a871eb
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-22 01:19:50 +0300

    creating router

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import FooterComponent from "./components/FooterComponent"
 import HeaderComponent from "./components/HeaderComponent"
 import ListEmployeeComponent from "./components/ListEmployeeComponent"
@@ -6,9 +7,16 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent"
 function App() {
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent />
-      <ListEmployeeComponent />
+        <Routes>
+          {/* Http//:localhost:3000 */}
+          <Route path='/' element = {<ListEmployeeComponent />}></Route>
+          {/* Http//:localhost:3000/employees */}
+          <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
+        </Routes>
       <FooterComponent />
+    </BrowserRouter>
     </>
   )
 }
