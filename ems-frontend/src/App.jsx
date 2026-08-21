dbffb872d37a583357a5cbfa41f6e4565a328c7b
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-21 20:58:42 +0300

    first commit

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import FooterComponent from "./components/FooterComponent"
+import HeaderComponent from "./components/HeaderComponent"
+import ListEmployeeComponent from "./components/ListEmployeeComponent"
+
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+      <ListEmployeeComponent />
+      <FooterComponent />
+    </>
+  )
+}
+
+export default App
